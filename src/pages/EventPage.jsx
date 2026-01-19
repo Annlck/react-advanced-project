c57dc704def3b9427322747ccd78f16a7a2c055b
Ann1ck <annick@henkes.it>
2026-01-19 12:09:07 +0100

    Add EditEventDialog

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -15,6 +15,8 @@ import { useLoaderData, Link } from "react-router-dom";
 import { useContext } from "react";
 import { Time } from "../components/Time";
 import { EventsContext } from "../EventsContext";
+import EditEvent from "../components/EditEvent";
+import { useState } from "react";
 
 export const loader = async ({ params }) => {
   const event = await fetch(`http://localhost:3000/events/${params.eventId}`);
@@ -25,65 +27,86 @@ export const EventPage = () => {
   scrollTo(top);
   const { event } = useLoaderData();
   const { deleteEvent, matchCategories } = useContext(EventsContext);
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
-    <Center>
-      <Box
-        w={{ md: "80vw" }}
-        borderWidth="1px"
-        m={{ base: "4", md: "10" }}
-        rounded="xl"
-        overflow="hidden"
-        shadow="xl"
-        position="relative"
-      >
-        <Link to="/">
-          <Button
-            position="absolute"
-            top="10px"
-            left="10px"
-            variant="subtle"
-            rounded="full"
-            size={{ base: "xs", md: "md" }}
-          >
-            <LuChevronLeft />
-            Go Back
-          </Button>
-        </Link>
+    <>
+      <EditEvent
+        open={modalOpen}
+        onClose={() => {
+          setModalOpen(false);
+        }}
+        finish={() => {
+          setModalOpen(false);
+        }}
+        eventToEdit={event}
+      />
 
-        <Image src={event.image} w="full" h="16rem" objectFit="cover" />
+      <Center>
+        <Box
+          w={{ md: "80vw" }}
+          borderWidth="1px"
+          m={{ base: "4", md: "10" }}
+          rounded="xl"
+          overflow="hidden"
+          shadow="xl"
+          position="relative"
+        >
+          <Link to="/">
+            <Button
+              position="absolute"
+              top="10px"
+              left="10px"
+              variant="subtle"
+              rounded="full"
+              size={{ base: "xs", md: "md" }}
+            >
+              <LuChevronLeft />
+              Go Back
+            </Button>
+          </Link>
 
-        <Box p={{ base: "8", md: "16" }}>
-          <Heading>{event.title}</Heading>
-          <Text>{event.description}</Text>
+          <Image src={event.image} w="full" h="16rem" objectFit="cover" />
 
-          <Separator my={{ base: "4", lg: "6" }} />
-          <Text>{event.location}</Text>
-          <HStack>
-            {event.startTime.length > 5 ? (
-              <Time timestamp={event.startTime} />
-            ) : (
-              <Text> {event.startTime} </Text>
-            )}
-            <Text> - </Text>
-            {event.endTime.length > 5 ? (
-              <Time timestamp={event.endTime} />
-            ) : (
-              <Text> {event.endTime} </Text>
-            )}
-          </HStack>
+          <Box p={{ base: "8", md: "16" }}>
+            <Heading>{event.title}</Heading>
+            <Text>{event.description}</Text>
 
-          <Flex>
-            {event.categoryIds.map((id) => (
-              <Tag.Root key={id} size="lg" mr="1" my="1" colorPalette="teal">
-                <Tag.Label>{matchCategories(id)}</Tag.Label>
-              </Tag.Root>
-            ))}
-          </Flex>
+            <Separator my={{ base: "4", lg: "6" }} />
+            <Text>{event.location}</Text>
+            <HStack>
+              {event.startTime.length > 5 ? (
+                <Time timestamp={event.startTime} />
+              ) : (
+                <Text> {event.startTime} </Text>
+              )}
+              <Text> - </Text>
+              {event.endTime.length > 5 ? (
+                <Time timestamp={event.endTime} />
+              ) : (
+                <Text> {event.endTime} </Text>
+              )}
+            </HStack>
 
-          <Button onClick={() => deleteEvent(event.id)}>Delete Event</Button>
+            <Flex>
+              {event.categoryIds.map((id) => (
+                <Tag.Root key={id} size="lg" mr="1" my="1" colorPalette="teal">
+                  <Tag.Label>{matchCategories(id)}</Tag.Label>
+                </Tag.Root>
+              ))}
+            </Flex>
+
+            <Button onClick={() => deleteEvent(event.id)}>Delete Event</Button>
+            <Button
+              onClick={() => {
+                setModalOpen(true);
+              }}
+            >
+              Edit Event
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Center>
+      </Center>
+    </>
   );
 };
